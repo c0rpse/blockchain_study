101fb90a1f7604e7ff87c0a18ff92b4e96ca94b4
wangdahao <wangdahao@baidu.com>
2018-08-21 11:33:14 +0800

    Update

---
+++ MPT_ENCODE.py
@@ -0,0 +1,17 @@
+def compact_encode(hexarray):
+    term = 1 if hexarray[-1] == 16 else 0
+    if term: hexarray = hexarray[:-1]
+    oddlen = len(hexarray) % 2
+    flags = 2 * term + oddlen
+    if oddlen:
+        hexarray = [flags] + hexarray
+    else:
+        hexarray = [flags] + [0] + hexarray
+    # hexarray now has an even length whose first nibble is the flags.
+    o = ''
+    for i in range(0,len(hexarray),2):
+        o += chr(16 * hexarray[i] + hexarray[i+1])
+    return o
+
+if __name__ == "__main__":
+    print(compact_encode([1,2,3,4,5,6]))
